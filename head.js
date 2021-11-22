fbebb328eee75275256510cbbee4a49326569071
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-22 23:41:11 +0000

    Add head.js

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function (arr) {
+  let a = arr.splice(0, 1);
+  return a[0];
+};
+
+// TEST CODE
+console.log(head([5, 6, 7]));
+console.log(head(["Hello", "Lighthouse", "Labs"]));
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([6]), 6);
