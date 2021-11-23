390b5723ea9427023d1e74bff4938b2434415ba8
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-23 21:50:30 +0000

    Created assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertArraysEqual(["hello", 1, "3"], ["hello", 1, "3"]);
+assertArraysEqual(["hello", "great", "3"], ["hello", "gret", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
