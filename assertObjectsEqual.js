852644feb8a7856f5ee5e6a6cd10b64144343d1e
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-25 02:43:58 +0000

    Implement assertObjectsEqual which will take in two objects and console.log an appropriate message to the console.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,52 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key of Object.keys(object1)) {
+      if (Array.isArray(object1[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" });
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" });
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4] });
