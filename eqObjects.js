9aa05d139b0df69aa2b36edd0dd11f7501a8122d
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 03:15:11 +0000

    Refactoring the code

--- eqObjects.js
+++ eqObjects.js
@@ -26,7 +26,7 @@ const eqObjects = function (object1, object2) {
       if (Array.isArray(object1[key])) {
         return eqArrays(object1[key], object2[key]);
       } else if (object1[key] instanceof Object) {
-        return eqObjects(object1[key], object2[key]);
+        if (!eqObjects(object1[key], object2[key])) return false;
       } else if (object1[key] !== object2[key]) {
         return false;
       }
@@ -54,7 +54,10 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
 
 assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
-
+assertEqual(
+  eqObjects({ a: { z: 1 }, b: { z: 3 } }, { a: { z: 1 }, b: 2 }),
+  false
+);
 assertEqual(
   eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),
   false
