0de109a05ea276900b37f5626801c1e6e8b46eac
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 21:22:50 +0000

    Add README.md

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
