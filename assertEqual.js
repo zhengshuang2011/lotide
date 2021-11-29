2f6c2bee3c8b960d346e9665c4e79891d79447cd
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 19:13:49 +0000

    Add test files

--- assertEqual.js
+++ assertEqual.js
@@ -4,8 +4,4 @@ const assertEqual = function (actual, expected) {
   } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const head = function (arr) {
   return arr[0];
 };
 
-// TEST CODE
-console.log(head([5, 6, 7]));
-console.log(head(["Hello", "Lighthouse", "Labs"]));
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([6]), 6);
+module.exports = head;
