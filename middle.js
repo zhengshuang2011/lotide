c1a0ee6981afd07400a40ebaebe799e48eda9f3b
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 19:39:37 +0000

    Add test files for middle.js and refactor the code

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = (array) => {
   const len = array.length;
   let middlePos = Math.trunc(len / 2);
@@ -31,10 +13,4 @@ const middle = (array) => {
   return output;
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
