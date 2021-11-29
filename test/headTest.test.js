1bf4b54f41966e981126e0baa992f6d5556d2aaa
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 19:55:58 +0000

    Refactor test files

--- test/headTest.test.js
+++ test/headTest.test.js
@@ -1,8 +1,18 @@
-const assertEqual = require("../assertEqual");
 const head = require("../head");
+const assert = require("chai").assert;
 
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([6]), 6);
+describe("#head", () => {
+  it("should return 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return 6 for [6]", () => {
+    assert.strictEqual(head([6]), 6);
+  });
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("should return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
