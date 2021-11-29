77e327d50e4081e4ee0294666826af83b022b231
Miranda Zheng <zhengshuang2011@gmail.com>
2021-11-29 03:16:14 +0000

    Refactoring the code with Recursive

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,11 @@ const eqArrays = (arr1, arr2) => {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -27,3 +31,17 @@ assertEqual(eqArrays(["hello", "great", "3"], ["hello", "gret", "3"]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+console.log("--------------------------");
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  ),
+  false
+); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
